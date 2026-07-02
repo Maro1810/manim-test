88c8fb85167e12cc5daac197601ba6d0280bf67a
Maro1810 <145640219+Maro1810@users.noreply.github.com>
2026-07-02 16:08:57 -0700

    first scene done

--- test.py
+++ test.py
@@ -25,31 +25,57 @@ class SectorScene(Scene):
 
         arc_length = MathTex(r"s=\frac{\theta}{2\pi}\cdot 2\pi r").move_to([1.5,-1.7, 0]).shift(shift)
 
+        slash = Line([2.7, 0.7, 0], [1, 0, 0],
+                     color = RED).move_to(arc_length.get_center()).shift(RIGHT*0.2, DOWN*0.2)
+
+        # self.add_sound("in depth vid 14.m4a")
+
+        self.wait(0.7)
         self.play(Create(sector), 
                   run_time = 0.5)
-        self.wait(0.2)
+        
+        self.wait(0.8)
 
         self.play(Create(r))
 
-        self.play(Create(arc))
+        self.play(Create(arc),
+                  run_time = 0.3)
 
         self.play(Create(theta))
+
+        self.wait(1)
+
         self.play(Create(s))
+
+        self.wait(2.3)
 
         self.play(Create(circumference), 
                   run_time = 0.5,
                   rate_func = linear)
         
+        self.wait(1)
+        
         self.play(Create(arc_length),
-                  run_time = 1.0,
+                  run_time = 1.5,
                   rate_func = linear)
+        
+        self.wait(3)
+        
+        self.play(Create(slash),
+                  run_time = 0.2)
         
         self.wait(1)
 
-        # arc_length = MathTex(r"s=r\theta")
+        self.play(FadeOut(arc_length, slash),
+                  run_time = 0.4)
 
-        # self.play(Create(arc_length),
-        #           run_time = 0.2,
-        #           rate_func = linear)
+        arc_length = MathTex(r"s=r\theta").shift(arc_length.get_center())
 
-        self.wait(3)
+        self.play(Create(arc_length),
+                  run_time = 0.5,
+                  rate_func = linear)
+
+        self.wait(1.4)
+
+        self.play(*[FadeOut(mob) for mob in self.mobjects], 
+                  run_time = 0.7)
